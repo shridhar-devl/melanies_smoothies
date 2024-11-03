3cf66a7acba694448b177ee733e49c75b4186bb7
shridhar-devl <shridhar1011@gmail.com>
2024-11-03 04:34:04 +0100

    Update streamlit_app.py
    d

--- streamlit_app.py
+++ streamlit_app.py
@@ -40,4 +40,4 @@ if ingredients_list:
 #New section to display fruit wise nutirtion information
 import requests
 fruityvice_response = requests.get("https://fruityvice.com/api/fruit/watermelon")
-st.text(fruityvice_response)
+st.text(fruityvice_response.json())
